70c6b3c9c44d892f7c3bff0da3325d8c63353df7
Travis Swicegood <development@domain51.com>
2012-03-03 17:11:07 -0600

    switch everything over to TextFields since it's all freeform

--- project/nc_viewer/models.py
+++ project/nc_viewer/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Entrant(models.Model):
-    name = models.CharField(max_length=250)
-    organization = models.CharField(max_length=250)
-    twitter = models.CharField(max_length=250)
-    country = models.CharField(max_length=250)
+    name = models.TextField()
+    organization = models.TextField()
+    twitter = models.TextField()
+    country = models.TextField()
 
 
 class Entry(models.Model):
     url = models.URLField()
     invalid = models.BooleanField(default=False)
-    title = models.CharField(max_length=250, default="")
+    title = models.TextField(default="")
     what = models.TextField(default="")
     who_else = models.TextField(default="")
     description = models.TextField(default="")
@@ -20,9 +20,9 @@ class Entry(models.Model):
     already_done = models.TextField(default="")
     sustain = models.TextField(default="")
 
-    amount = models.CharField(max_length=250, default="")  # Maybe they're asking for lots?
-    time = models.CharField(max_length=250, default="")
-    total_cost = models.CharField(max_length=250, default="")
+    amount = models.TextField(default="")  # Maybe they're asking for lots?
+    time = models.TextField(default="")
+    total_cost = models.TextField(default="")
 
     # Allow to be empty, because some people can't fill out a form
     entrant = models.ForeignKey(Entrant, null=True, blank=True)
