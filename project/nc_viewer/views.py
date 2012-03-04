fc1125a3316e5b3885c88fb1097881609ac0b0ae
Travis Swicegood <development@domain51.com>
2012-03-04 13:07:36 -0600

    add a d3 visualization of number of entries

--- project/nc_viewer/views.py
+++ project/nc_viewer/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.views.generic.list import ListView
 
 from . import models
@@ -6,10 +7,27 @@ from . import models
 class MainPageView(ListView):
     queryset = models.Entry.objects.filter(invalid=False)
 
+    def get_entry_totals(self):
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT
+                date_trunc('day', created_on) AS created_on_day,
+                COUNT(*)
+            FROM nc_viewer_entry
+            GROUP BY created_on_day
+            ORDER BY created_on_day
+        """)
+        # TODO: Isn't there an opposite of zip()?
+        return cursor.fetchall()
+
+    def as_javascript(self, data):
+        return "{%s}" % ", ".join([('"%s": %d' % (k, v)) for k, v in data])
+
     def get_context_data(self, *args, **kwargs):
         context = super(MainPageView, self).get_context_data(*args, **kwargs)
         context.update({
             "entries_count": models.Entry.objects.all().count(),
             "invalid_entries_count": models.Entry.objects.filter(invalid=True).count(),
+            "js_totals": self.as_javascript(self.get_entry_totals()),
         })
         return context
